4207c68e878e010f1d4ee94805d060481e9b6840
AndreySchmidt <ringnull@gmail.com>
2021-09-29 10:53:37 +0300

    добавляю подключение к БД

--- src/main/java/com/ringnull/hibernate/MainApp.java
+++ src/main/java/com/ringnull/hibernate/MainApp.java
@@ -1,7 +1,19 @@
 package com.ringnull.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class MainApp {
     public static void main(String[] args) {
-        
+        // создаю подключение к БД (сюда Configuration() при автоимпорте приходят настройки из hibernate.properties
+        // для этого автоимпорт надо подключить, у меня видимо само подключилось)
+        // следи за импортами org.hibernate.cfg.Configuration
+
+        SessionFactory sessionFactory = new Configuration().buildSessionFactory();
+
+
+
+        sessionFactory.close();
+
     }
 }
